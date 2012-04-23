f1610cb08116d4a79b4ce54aea9208d65eae36ce
unknown <Moody@Moody-Laptop.(none)>
2012-04-22 22:07:59 -0400

    More work. Attempting to get season creation done. Partially working, need to consider not using forms.

--- js/global.js
+++ js/global.js
@@ -1,29 +1,61 @@
+var dataLoaded = false;
+
 var playerRoster = null;
 var seasonData = null;
 
 function launch_app()
 {
-	if (typeof localStorage !== 'undefined')
+	if (!dataLoaded)
 	{
-		// Load the data from localStorage
-		playerRoster = localStorage.getItem("playerRoster");
-		seasonData = localStorage.getItem("seasonData");
+		if (typeof localStorage !== 'undefined')
+		{
+			// Load the data from localStorage
+			playerRoster = localStorage.getItem("playerRoster");
+			seasonData = localStorage.getItem("seasonData");
+		}
+		
+		dataLoaded = true;
 	}
+}
+
+function clean_variables()
+{
+	playerRoster = null
+	seasonData = null;
+	
+	location.href = "index.html"
 }
 
 function create_new_season(form)
 {
-	var chosen_season = form['select-season'].value;
-	var chosen_year = form['select-year'].value;
+	var new_season = { season: form['select-season'].value, year : form['select-year'].value };
+	var duplicate = false;
 	
-	// Need to validate that the season does not already exist
+	// Now add the new season
 	
-	if (seasonData == null)
+	if (seasonData === null)
 	{
-		// If the season data was empty to begin with, then we have a completely new device
-		// with no prior recorded seasons.
-		
-		var new_season = { s_id: 0, data : { season: chosen_season, year : chosen_year } };
-		seasonData = new_season;
+		seasonData = new Array();
 	}
+
+	// Potential that the user is attempting to add the same season twice
+	// Do not allow this
+	
+	// TODO: Fix this. I don't like it. I'm just too tired to think of a better way currently.
+	for (var i=0; i < seasonData.length; i++)
+	{
+		if (_.isEqual(seasonData[i], new_season))
+		{
+			duplicate = true;
+			break;
+		}
+	}
+	if (duplicate === false)
+	{
+		seasonData[seasonData.length] = new_season;
+	}
+	else
+	{
+		alert("Attempting to add same season twice. Fix this later.");
+	}	
 }
